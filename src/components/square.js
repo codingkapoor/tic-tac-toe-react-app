c3281d1c8995e4dd6e6a95b288a56d19d842d5aa
codingkapoor <mailtoshivamk@gmail.com>
2017-10-30 16:02:42 +0530

    sqaure moved to functional component

--- src/components/square.js
+++ src/components/square.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../index.css';
 
-class Square extends Component {
-  render() {
+const Square = (props) => {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
-  }
 }
 
 export default Square;
